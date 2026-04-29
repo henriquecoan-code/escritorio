b92f60697323814255f3cd2cfcba3265d2702c56
henriquecoan-code <henriquecoan@hotmail.com>
2026-04-29 13:26:01 -0300

    Fix GitHub Pages firebase config script

---
+++ firebase-config.public.js
@@ -0,0 +1,10 @@
+// Configuracao publica para ambiente hospedado (GitHub Pages / Firebase Hosting).
+window.OB_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBGv2dTJYkJPWH0_tJEl1PKXGHrOhwuvZ8",
+  authDomain: "escritorio-7b94e.firebaseapp.com",
+  projectId: "escritorio-7b94e",
+  storageBucket: "escritorio-7b94e.firebasestorage.app",
+  messagingSenderId: "829292607963",
+  appId: "1:829292607963:web:7834830744e9e67e7f8ffe",
+  measurementId: "G-R3W4EC8KE4"
+};
