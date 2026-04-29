74c45955a1f7762f3abd12bbcb3305b5b9eba2b7
henriquecoan-code <henriquecoan@hotmail.com>
2026-04-28 23:27:11 -0300

    1.2 firebase

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Preencha com as credenciais do seu projeto Firebase.
+// Nao compartilhe este arquivo publicamente.
+// Exemplo:
+// window.OB_FIREBASE_CONFIG = {
+//   apiKey: "SUA_API_KEY",
+//   authDomain: "seu-projeto.firebaseapp.com",
+//   projectId: "seu-projeto",
+//   storageBucket: "seu-projeto.firebasestorage.app",
+//   messagingSenderId: "1234567890",
+//   appId: "1:1234567890:web:abcdef123456"
+// };
+
+window.OB_FIREBASE_CONFIG = null;
