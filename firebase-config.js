1e89b72747f2b45c5558527c2e9231f723a00797
henriquecoan-code <henriquecoan@hotmail.com>
2026-04-29 00:10:22 -0300

    1.3 firebase works
    regra de visualização pública

--- firebase-config.js
+++ firebase-config.js
@@ -1,24 +1,6 @@
-// Preencha com as credenciais do seu projeto Firebase.
+// Configuracao global lida por OB_Dashboard_Rede.html.
 // Nao compartilhe este arquivo publicamente.
-// Exemplo:
-// window.OB_FIREBASE_CONFIG = {
-//   apiKey: "SUA_API_KEY",
-//   authDomain: "seu-projeto.firebaseapp.com",
-//   projectId: "seu-projeto",
-//   storageBucket: "seu-projeto.firebasestorage.app",
-//   messagingSenderId: "1234567890",
-//   appId: "1:1234567890:web:abcdef123456"
-// };
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+window.OB_FIREBASE_CONFIG = {
   apiKey: "AIzaSyBGv2dTJYkJPWH0_tJEl1PKXGHrOhwuvZ8",
   authDomain: "escritorio-7b94e.firebaseapp.com",
   projectId: "escritorio-7b94e",
@@ -27,7 +9,3 @@ const firebaseConfig = {
   appId: "1:829292607963:web:7834830744e9e67e7f8ffe",
   measurementId: "G-R3W4EC8KE4"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
